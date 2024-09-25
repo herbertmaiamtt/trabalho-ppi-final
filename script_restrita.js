3277df04ef36ccf6239628eb19a357dab109f08c
Lucas <lucasgabriel.1234@hotmail.com>
2024-09-25 20:52:23 -0300

    ✨: Criação da página de redirecionamento

---
+++ script_restrita.js
@@ -0,0 +1,4 @@
+document.getElementById('logoffBtn').addEventListener('click', function() {
+    alert("Você foi deslogado.");
+    window.location.href = "login.html";  // Redireciona para a página de login
+});
